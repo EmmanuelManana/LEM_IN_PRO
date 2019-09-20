f059c795a7a73697ff8360d1010baaa1fd54dd5a
Emmanuel MANANA <emanana@c4r18s5.wethinkcode.co.za>
2019-09-20 09:59:34 +0200

    updating ants

--- TheColony2.c
+++ TheColony2.c
@@ -6,7 +6,7 @@
 /*   By: emanana <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/18 12:11:48 by emanana           #+#    #+#             */
-/*   Updated: 2019/09/20 09:44:42 by emanana          ###   ########.fr       */
+/*   Updated: 2019/09/20 09:58:12 by emanana          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,14 @@ void		start_ants(t_ants *ants, int num)
 
 	i = 0;
 	while (++i <= num)
-		add_ants(to->ants, num);
-	return (num);
+		add_ants(ants, num);
 }
-
+/*push and poping of ants*/
 int			move_ants(t_room *from, t_room *to)
 {
 	int		num;
 
-	num = remove_ants(to->ants, num);
-	add_ants(to->ants, num);
+	num = remove_ants(from->ants);/*pop from */
+	add_ants(to->ants, num);/*push to*/
 	return (num);
 }
